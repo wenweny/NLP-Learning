aa3aa974aa622cffb16c362cc204bd734e62e4a3
wenweny <465393572@qq.com>
2021-12-09 10:31:15 +0800

    add 116

---
+++ leetcode/code/116m_PopulatingNextRightPointers.py
@@ -0,0 +1,59 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2021/12/9 9:14
+# @Author  : yangwenwen
+"""
+@Function:116. 填充每个节点的下一个右侧节点指针
+
+给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+输入：root = [1,2,3,4,5,6,7]
+输出：[1,#,2,3,#,4,5,6,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+"""
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+
+        if not root:
+            return None
+        self.connect_two_node(root.left, root.right)
+        return root
+
+    def connect_two_node(self, node_1, node_2):
+        """定义递归子函数含义：连接传进去的每两个相邻节点"""
+        if not node_1 and not node_2:
+            return None
+        node_1.next = node_2
+        self.connect_two_node(node_1.left, node_1.right)
+        self.connect_two_node(node_2.left, node_2.right)
+        self.connect_two_node(node_1.right, node_2.left)
